6d593178029dc0a56c9c1e657f12fddeef4fd135
L0la <torok.gabi@gmail.com>
2021-02-10 17:08:12 +0100

    media query hozzáadása, style véglegesítése

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       <Content />
+      <div class="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+          Coded by <a href="https://github.com/torok-gabi" target="_blank">Török Gabi</a>.
+      </div>
     </div>
   );
 }
